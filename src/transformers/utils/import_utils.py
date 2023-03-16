ea6fe6b7926a18c310b17d4595eec47e1ac19dd0
Susnato Dhar <susnatodhar10@gmail.com>
2023-03-16 21:09:42 +0530

    .

--- src/transformers/utils/import_utils.py
+++ src/transformers/utils/import_utils.py
@@ -256,6 +256,20 @@ try:
 except importlib_metadata.PackageNotFoundError:
     _librosa_available = False
 
+_essentia_available = importlib.util.find_spec("essentia") is not None
+try:
+    _essentia_version = importlib_metadata.version("essentia")
+    logger.debug(f"Successfully imported essentia version {_essentia_version}")
+except importlib_metadata.PackageNotFoundError:
+    _essentia_version = False
+
+_pretty_midi_available = importlib.util.find_spec("pretty_midi") is not None
+try:
+    _pretty_midi_version = importlib_metadata.version("pretty_midi")
+    logger.debug(f"Successfully imported essentia version {_pretty_midi_version}")
+except importlib_metadata.PackageNotFoundError:
+    _pretty_midi_available = False
+
 ccl_version = "N/A"
 _is_ccl_available = (
     importlib.util.find_spec("torch_ccl") is not None
@@ -297,6 +311,11 @@ def is_pyctcdecode_available():
 def is_librosa_available():
     return _librosa_available
 
+def is_essentia_available():
+    return _essentia_available
+
+def is_pretty_midi_available():
+    return _pretty_midi_available
 
 def is_torch_cuda_available():
     if is_torch_available():
